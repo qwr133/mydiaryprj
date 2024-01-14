e8caa64b0d960ec895afd92e9adc6b83719a5851
qwr133 <ksg165000@gmail.com>
2024-01-14 18:24:16 +0900

    :sparkles: mypage 패키지 추가

---
+++ src/main/java/com/mydiary/mydiaryprj/mypageapi/api/MypageController.java
@@ -0,0 +1,24 @@
+package com.mydiary.mydiaryprj.mypageapi.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/member/mypage")
+@RestController
+@RequiredArgsConstructor
+public class MypageController {
+
+/*
+    1. 내정보조회
+    2. 내가 쓴 글 조회
+    3. 내가 쓴 댓글
+    4. 내정보 수정
+    5. 탈퇴하기
+
+ */
+    // final private MypageService mypageService;
+
+
+
+}
